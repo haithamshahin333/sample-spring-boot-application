5447643a5e4738d630c431e17dfb3fea16ce00c9
Haitham Shahin <hshahin@redhat.com>
2019-03-04 11:40:40 -0500

    updated

--- src/main/java/puckboard/sample/Application.java
+++ src/main/java/puckboard/sample/Application.java
@@ -2,6 +2,10 @@ package puckboard.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,17 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT",
+						"DELETE", "PATCH");
+			}
+		};
+	}
+
+	
 }
